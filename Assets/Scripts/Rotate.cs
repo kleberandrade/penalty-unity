9f52ed893c6b3e49e7b79190d6f654b6fbe6b0f3
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2019-11-30 10:03:16 -0300

    Modelagem do cenário

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [Range(-90.0f, 90.0f)]
+    public float m_Range;
+    public float m_SmoothTime;
+    public Vector3 m_Axis;
+    private Vector3 m_Origin;
+
+    private void Start()
+    {
+        m_Origin = transform.eulerAngles;
+    }
+
+    private void Update()
+    {
+        float period = (Time.time * (2.0f * Mathf.PI)) * m_SmoothTime;
+        Vector3 rotation = m_Axis * Mathf.Sin(period) * m_Range;
+        transform.localEulerAngles = m_Origin + rotation;
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public Rigidbody m_Bullet;
+    public float m_Force;
+    public Transform m_Point;
+    public float m_CooldownTime = 1.0f;
+
+    private float m_NextShootTime;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time >= m_NextShootTime)
+        {
+            var body = Instantiate<Rigidbody>(m_Bullet, m_Point.position, m_Point.rotation);
+            body.AddForce(m_Point.forward * m_Force, ForceMode.Impulse);
+            m_NextShootTime = Time.time + m_CooldownTime;
+        }
+    }
+}
